f21336fdc61d70c53d8f8d85e22afd890b7ee758
f0rbit <dev@forbit.dev>
2021-06-04 21:43:21 +0930

    main page blur background and hi there! card

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import About from './components/About';
-import Card from './components/Card';
+import Card from './components/Card'
 import Menu from './components/Menu'
+import Background from './assets/2fuS10.gif'
 
 function App() {
   return (
-      <div className="font-sans font-bold bg-bgGray">
-        <Menu />
-        <br />
-        <div className="bg-bgGray">
+      <div className="font-sans font-bold bg-bgGray overflow-x-hidden overflow-y-hidden overscroll-none h-screen">
+      
+        <div className="relative w-screen">
+          <Menu />
+        </div>
+        <div className="bg-bgGray relative">
+          <img className="object-fill object-center h-screen w-screen" src={Background} alt="Background"/>
+        </div>
+        <div className="fixed h-screen w-screen top-10 bg-gradient-to-r from-bgGray via-bgGray to-none opacity-95 backdrop-filter backdrop-blur-xl">
+          
+        </div>
+        <div>
           <Card />
-          <About />
-          <About />
-          <About />
-          <About />
-          <About />
         </div>
       </div>
   );
--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export default class Button extends Component {
     render() {
         return (
-        <div className="rounded-lg bg-gradient-to-r bg-bgGrayLight hover:from-forbitPink hover:to-forbitBlue px-3">
-            <button className="font-bold focus:outline-none">{this.props.id}</button>
+        <div className="rounded-lg bg-gradient-to-r bg-bgGrayLight hover:from-forbitPink hover:to-forbitBlue px-3 shadow-md">
+            <button className="font-sans font-semibold focus:outline-none">{this.props.id}</button>
         </div>
         )
     }
