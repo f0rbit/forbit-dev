f4d72be5366ec4b4fff125b1900184c180c43036
f0rbit <dev@forbit.dev>
2022-02-07 22:06:32 +1030

    load previous work
    Only the timeline has been completed.

---
+++ components/Timeline.js
@@ -0,0 +1,28 @@
+import CommitTimelineItem from "./CommitTimelineItem";
+import RedditTimelineItem from "./RedditTimelineItem";
+import TwitterTimelineItem from "./TwitterTimelineItem";
+export function getDate(date) {
+  var d = new Date(date);
+  return <p>{d.toLocaleString()}</p>;
+}
+
+function TimelineItem(data) {
+  if (data.category === "COMMITS") return <CommitTimelineItem commits={data} />;
+  if (data.category === "REDDIT") return <RedditTimelineItem post={data} />;
+  if (data.category === "TWITTER") return <TwitterTimelineItem post={data} />;
+  return <></>;
+}
+
+function Timeline({ timeline }) {
+  console.log(timeline);
+
+  return (
+    <div className="h-full min-w-[70%] max-w-[38rem] p-2">
+      <div className="flex flex-col items-center justify-center space-y-4">
+        {timeline.map((i) => TimelineItem(i))}
+      </div>
+    </div>
+  );
+}
+
+export default Timeline;
---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import NavBar from "../components/NavBar";
+import Typical from "react-typical";
+
+export default function index() {
+  return (
+    <div className=" overflow-x-hidden">
+      <div className=" flex h-screen w-screen flex-col items-center justify-center  space-y-3 bg-neutral-900">
+        <h1 className="font-sans text-8xl font-bold text-white">forbit</h1>
+        <div className="font-mono text-neutral-500">
+          <span className>I </span>
+          <Typical
+            steps={[
+              "am a developer.",
+              1000,
+              "am a game designer.",
+              1000,
+              "am an open source contributor.",
+              1000,
+              "study computer science.",
+              1000,
+              "am an artist",
+              1000,
+              "am a gamer.",
+              1000,
+            ]}
+            loop={Infinity}
+            wrapper="span"
+          />
+        </div>
+      </div>
+      <div className="h-full w-full">
+        <NavBar noicon="true" />
+      </div>
+      <div className="h-[96rem] w-full bg-neutral-800"></div>
+    </div>
+  );
+}
